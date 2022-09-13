f551b8e52dd2d5a037edb8d8003e812afaf900a9
Igor <igorcardosokaize@gmail.com>
2022-09-12 21:30:21 -0300

    Updated calculator. Went to stack overflow to get some help. I hope I don't get stoned to death there.

--- Electricity_Costs/Electricity_Costs_Calculator.py
+++ Electricity_Costs/Electricity_Costs_Calculator.py
@@ -16,53 +16,52 @@
 #That means 44129,9 x 8 x 12 / 1000, so 4412.99kWh. 
 
 def horas_trabalho():
-    global horas_uso
+    global horas_uso_motor
     global potencia_kw
     print("Quantas horas por mês o motor trabalhará?")
-    horas_uso = float(input(">: "))
+    horas_uso_motor = float(input(">: "))
     potencia_kw = 44129.9
     potencia_kwh()
 
 def potencia_kwh():
-    global total_kwh
-    total_kwh = (horas_uso * potencia_kw)/1000
-    print(f"Considerando um uso de {horas_uso} horas no mês, o consumo de eletricidade será de {total_kwh} kWh.\n")
+    global consumo_mensal_kwh
+    consumo_mensal_kwh = (horas_uso_motor * potencia_kw)/1000
+    print(f"Considerando um uso de {horas_uso_motor} horas no mês, o consumo de eletricidade será de {consumo_mensal_kwh} kWh.\n")
+    print("Agora vamos aos custos da energia elétrica... \n")
     custo_energia()
 
 def custo_energia():
 
     bandeiras = {
-        "Bandeira Verde" : "0.19829",
-        "Bandeira Amarela" : "0.22818",
-        "Bandeira Vermelha 1" : "0.26329",
-        "Bandeira Vermelha 2" : "0.29624",
-        "Bandeira Escassez" : "0.34029"
+        "Bandeira Verde" : "19829",
+        "Bandeira Amarela" : "22818",
+        "Bandeira Vermelha 1" : "26329",
+        "Bandeira Vermelha 2" : "29624",
+        "Bandeira Escassez" : "34029"
     }
 
-    print("As bandeiras de consumo de eletricidade são: \n")
+    print("As bandeiras de consumo de eletricidade da CEMIG são: \n")
 
     for x in bandeiras:
-        print (x, "\n", "-" * 20)
+        print (x, "\n", "-" * 20, "\n")
 
     def bandeira():
         while True:
-            print("Qual é a bandeira de consumo de energia na região do sítio?")
+            print("Qual é a bandeira de consumo de energia na região do sítio? \n")
+            global escolha_bandeira_atual
             escolha_bandeira_atual = input(">: ")
 
             if escolha_bandeira_atual in bandeiras:
-                print(f"Atualmente estamos em {escolha_bandeira_atual}")
+                print(f"Certo, então atualmente estamos em {escolha_bandeira_atual}. \n")
                 #I can't get this to work properly.
                 #I need a way to transform the value of the key to a variable, so I can multiply it.
                 #There are easier ways to do this, but I don't find them clean enough.
 
                 #It is assigining the key to a function, but not the key I want to.
-                for key in bandeiras:
-                    value = bandeiras[key]
+                for value in bandeiras[escolha_bandeira_atual]:
                     global custo_kwh
-                    global bandeira_atual
-                    custo_kwh = float(value)
-                    bandeira_atual = key
-                
+                    custo_kwh = int(value)
+
                 preco_eel()
 
             else:
@@ -71,22 +70,35 @@ def custo_energia():
     bandeira()
 
 def preco_eel():
-    total_eel = (custo_kwh * total_kwh)
-    print(f"""O valor do consumo mensal de energia elétrica, ao utilizar um motor elétrico de 45kW por {horas_uso} horas é de R$ {total_eel}, considerando o valor de R$ {custo_kwh} por kWh, em período de {bandeira_atual}. \n""")
+    print(f"Dessa maneira, o custo do kWh é de R$ 0,{custo_kwh}")
+    total_eel = (custo_kwh * consumo_mensal_kwh)
+    print(f"Isso significa que o valor do consumo mensal de energia elétrica, ao utilizar um motor elétrico de 45kW por {horas_uso_motor} horas é de R$ {total_eel}, considerando o valor de R$ {custo_kwh} por kWh, em período de {escolha_bandeira_atual}. \n")
+    print(f"Òtimo! Agora que sabemos os custos da energia elétrica, precisamos dar uma olhada no diesel. \n")
     custo_diesel()
 
 def custo_diesel():
-    print("Qual é o valor do litro do diesel atualmente?: \n")
+    print("Atualmente, qual é o valor do litro do diesel?: \n")
     preco_diesel = input(">: ")
     preco_diesel_conv = float(preco_diesel.replace(',', '.'))
     consumo_hora = 13
-    valor_diesel = (preco_diesel_conv * consumo_hora * horas_uso)
-    diesel_total = (consumo_hora * horas_uso)
+    valor_mensal_diesel = (preco_diesel_conv * consumo_hora * horas_uso_motor)
+    consumo_diesel_total = (consumo_hora * horas_uso_motor)
 
     #format(valor_diesel, '.2f') -> dar um jeito de formatar isso
-    print(f"""O valor mensal gasto atualmente em diesel é de aproximadamente R$ {valor_diesel}, considerando um valor de R$ {preco_diesel} por litro, com {horas_uso} horas de uso, incorrendo no consumo de {diesel_total} litros de diesel. \n""")
+    print(f"Legal! O valor mensal gasto atualmente em diesel é de aproximadamente R$ {valor_mensal_diesel}, considerando um valor de R$ {preco_diesel} por litro, com {horas_uso_motor} horas de uso, incorrendo no consumo de {consumo_diesel_total} litros de diesel. \n")
+    print("Calculados os custos com diesel e energia elétrica, podemos partir para as despesas com a conversão do sistema.")
+    custos_adicionais()
 
+def custos_adicionais():
 
-    
+    print("Insira o preço do motor elétrico")
+    motor_preco = float(input(">: "))
 
+    print("Insira o preço total dos cabos de eletricidade")
+    cabos_preco = float(input(">: "))
 
+    print("Insira o preço dos postes-padrão")
+    postes_preco = float(input(">: "))
+
+    print("Insira quaisquer valores adicionais não previstos pela calculadora")
+    adicionais_preco = float(input(">: "))
